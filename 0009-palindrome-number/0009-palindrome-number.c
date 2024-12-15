218bf6a66a18ac78a745df36784f31ee7c660492
Navara Sai Divakar <119742639+SaiDivakar-Navara@users.noreply.github.com>
2024-12-15 16:15:44 +0530

    Time: 1 ms (54.71%), Space: 8.1 MB (41.14%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -0,0 +1,15 @@
+bool isPalindrome(int x){
+{
+  int t=x;
+  unsigned int sum=0;
+  int r=0;
+  while(x>0)
+  {
+     r=x%10;
+     sum=(sum*10)+r;
+     x=x/10;
+  }
+  if (sum==t) return 1;
+  else return 0;
+}
+}
