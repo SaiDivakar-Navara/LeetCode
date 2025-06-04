166de26b1f430ca0b9b22b40182890a16f061517
Navara Sai Divakar <119742639+SaiDivakar-Navara@users.noreply.github.com>
2025-06-04 10:31:30 +0530

    Time: 599 ms (74.47%), Space: 19.6 MB (55.23%) - LeetHub

---
+++ 3561-resulting-string-after-adjacent-removals/3561-resulting-string-after-adjacent-removals.py
@@ -0,0 +1,9 @@
+class Solution:
+    def resultingString(self, s: str) -> str:
+        st = []
+        for ch in s:
+            if st and (abs(ord(st[-1]) - ord(ch)) == 1 or abs(ord(st[-1]) - ord(ch)) == 25):
+                st.pop()
+            else:
+                st.append(ch)
+        return ''.join(st)        
